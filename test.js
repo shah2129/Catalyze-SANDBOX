15bde601223293f95eeba0f12c20dfceb1a70619
Hasit Shah <Hasit.Shah@stonybrookmedicine.edu>
2017-02-15 10:25:22 -0500

    initial commit

---
+++ test.js
@@ -0,0 +1,11 @@
+var http = require('http');
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World from Hasit at "+Date());
+});
+// Listen on port 8000 or env
+var port = process.env.PORT || 8000
+server.listen(port);
+// Put a friendly message on the terminal
+console.log("listenign to port "+port);
